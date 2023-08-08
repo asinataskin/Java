f16e48cfa8931790a069cd2bb55182c87cce146e
asinataskin <86055273+asinataskin@users.noreply.github.com>
2023-08-08 13:43:25 +0300

    java_sample

--- DeadLock/src/Hesap.java
+++ DeadLock/src/Hesap.java
@@ -1,11 +1,25 @@
+
 public class Hesap {
-    private int bakiye = 1000;
-
-    public void paraCek(int paracek){
-        bakiye -=paracek;
-
+    private int bakiye = 10000;
+    
+    public void paraCek(int miktar) {
+        bakiye -= miktar;
+        
     }
-    public void paraYatır(int paraYatır){
-        bakiye += paraYatır;
+    public void paraYatir(int miktar) {
+        bakiye += miktar;
+    }
+    public static void paraTransferi(Hesap hesap1,Hesap hesap2,int miktar) {
+        hesap1.paraCek(miktar);
+        hesap2.paraYatir(miktar);
+        
+    }
+
+    public int getBakiye() {
+        return bakiye;
+    }
+
+    public void setBakiye(int bakiye) {
+        this.bakiye = bakiye;
     }
 }
